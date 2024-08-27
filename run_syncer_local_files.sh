4f5f0f2110c49bb7b34f38baafea9f1d8ed2622f
cosmingamanusi <cosmin.gamanusi@gmail.com>
2024-08-27 22:05:09 +0000

    README.md

--- run_syncer_local_files.sh
+++ run_syncer_local_files.sh
@@ -5,5 +5,5 @@ sudo docker build -t solana-syncer .
 sudo docker run --rm --name solana-syncer-job \
 	-e JVM_ARGS="-Xmx26g" \
 	-v ./config.properties:/app/config.properties \
-	-v ./data:/data \
+	-v /data:/data \
 	solana-syncer read-source=local-files
